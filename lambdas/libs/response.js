5576f537d9911d36c01df0cba8b2b1bed399f3d3
Gilbert <gilwong3105@gmail.com>
2020-08-18 01:13:07 -0700

    Initial project setup

---
+++ lambdas/libs/response.js
@@ -0,0 +1,12 @@
+const handlerResponse = (statusCode, payload) => {
+  return {
+    statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(payload),
+  };
+};
+
+export default handlerResponse;
