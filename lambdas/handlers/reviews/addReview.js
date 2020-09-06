36e40482d07d5309a0086fe9d61f8617bd4c6004
Gilbert <gilwong3105@gmail.com>
2020-09-06 16:43:04 -0700

    Create addReview lambda

---
+++ lambdas/handlers/reviews/addReview.js
@@ -0,0 +1,32 @@
+import { handlerResponse, client } from '../../libs';
+import { validateReview } from '../../models/review';
+
+export const add = async event => {
+  try {
+    const { value, error } = validateReview(JSON.parse(event.body) ?? {});
+
+    if (error) {
+      return handlerResponse(400, error);
+    }
+
+    const params = {
+      TableName: process.env.restaurantTable,
+      Key: { id: event.pathParameters.id },
+      ReturnValues: 'ALL_NEW',
+      UpdateExpression:
+        'set #reviews = list_append(if_not_exists(#reviews, :empty), :newReviews)',
+      ExpressionAttributeNames: {
+        '#reviews': 'reviews'
+      },
+      ExpressionAttributeValues: {
+        ':newReviews': [value],
+        ':empty': []
+      }
+    };
+
+    const { Attributes } = await client.update(params);
+    return handlerResponse(200, Attributes);
+  } catch (err) {
+    return handlerResponse(500, err.message ?? '');
+  }
+};
